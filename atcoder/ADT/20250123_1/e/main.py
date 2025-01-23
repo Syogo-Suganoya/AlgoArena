30104f8abb116095759fe34645efd32f25f99585
Syogo-Suganoya <registration@ssuganoya.com>
2025-01-23 19:15:47 +0900

    fix スキップした問題のコードを削除

--- atcoder/ADT/20250123_1/e/main.py
+++
@@ -1,8 +0,0 @@
-Ha, Wa = map(int, input().split())  # 迷路の縦と横のサイズ
-maze_a = [list(input()) for _ in range(Ha)]
-
-Hb, Wb = map(int, input().split())  # 迷路の縦と横のサイズ
-maze_b = [list(input()) for _ in range(Hb)]
-
-Hx, Wx = map(int, input().split())  # 迷路の縦と横のサイズ
-maze_x = [list(input()) for _ in range(Hx)]
