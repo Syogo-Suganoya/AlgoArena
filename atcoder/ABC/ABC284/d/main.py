14e0bf2d93e1726bac0bc393df1f3b977c0015ee
Syogo-Suganoya <registration@ssuganoya.com>
2025-07-19 19:37:10 +0900

    fix ABC284 d

--- atcoder/ABC/ABC284/d/main.py
+++ atcoder/ABC/ABC284/d/main.py
@@ -1,26 +1,19 @@
 import math
 
 T = int(input())
-
 for _ in range(T):
     N = int(input())
-
-    # N = p^2 * q か N = p * q^2 という形で分解できるか調べる
-    for x in range(2, int(1e6) + 1):
-        # まず、x^2 が N を割り切れる場合を調べる
-        if N % (x * x) == 0:
-            p = x
-            q = N // (x * x)
-            # 形: N = p^2 * q
+    limit = int(N ** (1 / 3)) + 2  # およその3乗根（余裕を持って +2）
+    for p in range(2, limit):
+        if N % p == 0:
+            # p が小さい方の素数候補
+            pq = N // p
+            if pq % p == 0:
+                # p^2 が割り切れる場合：q = pq // p
+                q = pq // p
+            else:
+                # p は q 側、q = pq
+                q = p
+                p = int(math.isqrt(pq))
             print(p, q)
             break
-        # それがダメなら、x が N を割り切れる場合を調べる
-        elif N % x == 0:
-            q = x
-            p_sq = N // x
-            # ここで、p^2 = N / q であることを確認
-            p = int(math.isqrt(p_sq))
-            if p * p == p_sq:
-                # 形: N = q * p^2
-                print(p, q)
-                break
