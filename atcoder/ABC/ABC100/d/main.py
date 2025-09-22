1ab6546c60ff9ef8b60ab4fcbe96b3001bd7e7a9
Syogo-Suganoya <registration@ssuganoya.com>
2025-09-23 06:26:36 +0900

    add ABC100 D

--- atcoder/ABC/ABC100/d/main.py
+++ atcoder/ABC/ABC100/d/main.py
@@ -1,1 +1,22 @@
-# 水のため、スキップ
+N, M = map(int, input().split())
+X, Y, Z = [], [], []
+for _ in range(N):
+    x, y, z = map(int, input().split())
+    X.append(x)
+    Y.append(y)
+    Z.append(z)
+
+INF = 10**18
+ans = -INF
+# a, b, c の符号を全探索 (-1 or +1)
+for a in [-1, 1]:
+    for b in [-1, 1]:
+        for c in [-1, 1]:
+            # 各ケーキの「価値」を計算
+            vals = [a * X[i] + b * Y[i] + c * Z[i] for i in range(N)]
+            # 大きい順に M 個選べばよい
+            vals.sort(reverse=True)
+            total = sum(vals[:M])
+            ans = max(ans, total)
+
+print(ans)
