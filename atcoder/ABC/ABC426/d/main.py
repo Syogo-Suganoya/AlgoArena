7211167aafbc20e222c371faa483d84cf0ee8d90
Syogo-Suganoya <registration@ssuganoya.com>
2025-10-05 13:58:38 +0900

    add ABC426

--- atcoder/ABC/ABC426/d/main.py
+++ atcoder/ABC/ABC426/d/main.py
@@ -13,11 +13,15 @@ for _ in range(T):
             r += 1
         c = int(S[l])
         length = r - l
-        mx[c] = max(mx[c], length)
-        cnt[c] += length
+        mx[c] = max(mx[c], length)  # 文字 c の最長連続長を更新
+        cnt[c] += length  # 文字 c の総出現数
         l = r
 
     ans = 2 * N
     for i in range(2):
+        # i = 0 または 1 を全て揃える場合の操作回数を計算
+        # (cnt[i] - mx[i]) → 長さが揃わない部分を移動
+        # 2倍するのは両端から操作可能だから
+        # cnt[i ^ 1] → 揃えたい文字以外の文字を動かす必要
         ans = min(ans, (cnt[i] - mx[i]) * 2 + cnt[i ^ 1])
     print(ans)
