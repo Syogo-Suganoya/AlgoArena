3802a54f52530dafe5589d2cbd4f54b2bea1f57a
Syogo-Suganoya <registration@ssuganoya.com>
2025-11-04 12:59:11 +0900

    fix ABC142 D

--- atcoder/ABC/ABC142/d/main.py
+++ atcoder/ABC/ABC142/d/main.py
@@ -1,17 +1,14 @@
 import math
 
+from sympy import factorint
+
 A, B = map(int, input().split())
+
+# 最大公約数を求める
 g = math.gcd(A, B)
 
-cnt = 0
-i = 2
-while i * i <= g:
-    if g % i == 0:
-        cnt += 1
-        while g % i == 0:
-            g //= i
-    i += 1
-if g > 1:
-    cnt += 1
+# 素因数分解 (返り値は {素数: 指数} の辞書)
+factors = factorint(g)
 
-print(cnt + 1)  # 1 を含めて
+# 種類数 + 1 が答え
+print(len(factors) + 1)
