fbaafc9ac8948a088b4264c018faed01d640bfc0
Syogo-Suganoya <registration@ssuganoya.com>
2026-03-14 17:15:19 +0900

    fix ABC318 E

--- atcoder/ABC/ABC318/e/main.py
+++ atcoder/ABC/ABC318/e/main.py
@@ -1,19 +1,28 @@
 N = int(input())
 A = list(map(int, input().split()))
 
-# 値ごとに累積の「添字の合計(sum)」と「出現回数(size)」を管理
-sum_dict = {}
-size_dict = {}
+MAX = max(A) + 1
 
+left = [0] * MAX
+right = [0] * MAX
+
+for a in A:
+    right[a] += 1
+
+tmp = 0
 ans = 0
 
-for k, x in enumerate(A, start=1):  # k は 1 から N
-    s = size_dict.get(x, 0)
-    sm = sum_dict.get(x, 0)
+for i in range(N):
+    a = A[i]
 
-    ans += (k - 1) * s - sm - (s * (s - 1) // 2)
+    tmp -= left[a] * right[a]
 
-    size_dict[x] = s + 1
-    sum_dict[x] = sm + k
+    right[a] -= 1
+
+    ans += tmp
+
+    left[a] += 1
+
+    tmp += left[a] * right[a]
 
 print(ans)
