ab3c5a0f628874676bd285df313242bac9ddde64
Syogo-Suganoya <registration@ssuganoya.com>
2026-03-14 17:15:06 +0900

    fix ABC324 E

--- atcoder/ABC/ABC324/e/main.py
+++ atcoder/ABC/ABC324/e/main.py
@@ -1,47 +1,34 @@
+import bisect
+
 N, T = input().split()
 N = int(N)
 S = [input() for _ in range(N)]
 M = len(T)
 
+pref = []
+suf = []
 
-# 各文字列の prefix 長を求める
-def prefix_len(s, t):
-    """sの中でtのprefixをどこまで拾えるか"""
-    j = 0
+for s in S:
+    # prefix
+    p = 0
     for c in s:
-        if j < len(t) and c == t[j]:
-            j += 1
-    return j
+        if p < M and c == T[p]:
+            p += 1
+    pref.append(p)
 
-
-# 各文字列の suffix 長を求める
-def suffix_len(s, t):
-    """sの中でtのsuffixをどこまで拾えるか"""
-    j = 0
+    # suffix
+    q = 0
     for c in reversed(s):
-        if j < len(t) and c == t[-1 - j]:
-            j += 1
-    return j
+        if q < M and c == T[M - 1 - q]:
+            q += 1
+    suf.append(q)
 
+suf.sort()
 
-prefixes = [prefix_len(s, T) for s in S]
-suffixes = [suffix_len(s, T) for s in S]
-
-# suffix 長の出現回数を集計
-cnt = [0] * (M + 1)
-for b in suffixes:
-    cnt[b] += 1
-
-# 後ろから累積和: cnt_ge[x] = suffix長 >= x の個数
-cnt_ge = [0] * (M + 2)
-cnt_ge[M] = cnt[M]
-for i in range(M - 1, -1, -1):
-    cnt_ge[i] = cnt_ge[i + 1] + cnt[i]
-
-# ペアを数える
 ans = 0
-for a in prefixes:
-    need = M - a  # 残り必要なsuffixの長さ
-    ans += cnt_ge[need]
+for p in pref:
+    need = M - p
+    idx = bisect.bisect_left(suf, need)
+    ans += len(suf) - idx
 
 print(ans)
